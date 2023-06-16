77af4d2c72e926bb858c1d4e9f08977c333eaf29
Carlos && Ponce <srtager555@gmail.com>
2023-06-15 22:36:32 -0600

    Main features added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { FeaturedProduct } from "@/components/home/FeaturedProduct";
+import { ProductList } from "@/components/home/ProductList";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +16,10 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="https://i.ibb.co/X5Wg7S5/kda.jpg" />
 			</Head>
+			<FeaturedProduct />
+			<ProductList />
+			<ProductList />
+			<ProductList />
 		</>
 	);
 }
