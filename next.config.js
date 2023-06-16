1c3ae5ca2294f717bd7c4d1c1893abecaefeb930
Carlos && Ponce <srtager555@gmail.com>
2023-06-15 22:07:17 -0600

    next configuration updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	compiler: {
+		styledComponents: true,
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "i.ibb.co",
+				port: "",
+				pathname: "**",
+			},
+		],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
